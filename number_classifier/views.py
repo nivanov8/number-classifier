06528e859a3c82a70926591edd3a1a3c603c4b60
nivanov8 <nickivanov8@icloud.com>
2022-05-01 21:14:27 -0400

    new

--- number_classifier/views.py
+++ number_classifier/views.py
@@ -8,7 +8,7 @@ from .Neural_Network.predict import Predict
 import base64
 from io import BytesIO
 
-# landing page
+# main page 
 def index(request):
     if request.method == "GET":
         return render(request, "index.html")
